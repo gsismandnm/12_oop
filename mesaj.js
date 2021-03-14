323d4e9b6733e8119a1a482165608fe8ec409611
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-03-14 17:55:52 +0200

    Update 2

--- mesaj.js
+++ mesaj.js
@@ -2,17 +2,40 @@ class Bildirim {
     gonder = () => {
         var eposta = new EpostaBildirim();
         eposta.gonder();
+
         console.log('Bildirim yapildi');
     }
 }
 
-class EpostaBildirim {
-    gonder = () => {
-        console.log ('Eposta gönderildi.');
+class Mesaj {
+    constructor (mesaj){
+        this.mesaj = mesaj;
+
     }
 }
 
+
+
+class EpostaBildirim  extends Mesaj{
+    gonder = () => {
+        console.log ('Eposta gönderildi.');
+    }
+    gonder = () => {
+        console.log ('_________________');
+        console.log (`Bu mesaj bir ${this.mesaj}dir.`)
+
+    }
+}
+
+
 //BUTANA BASILILARAK ÇALIŞTIRDIK.
+
+const buttonClicked = () => {
+    var bildirim =new Bildirim ();
+    bildirim.gonder();
+}
+
+buttonClicked();
 
 var bildirim = new Bildirim();
 bildirim.gonder();
